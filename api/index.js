9ae12c9118ae1bb6ca2c5392ef260dfe06baefe5
Prateek-2022 <patricklucifer9@gmail.com>
2023-08-23 22:45:09 +0530

    redirect to home route added

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const { default: mongoose } = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "jhagjhgsakgfjhgask";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 app.use(express.json());
 
@@ -25,6 +27,20 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (e) {
     res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
   }
 });
 
