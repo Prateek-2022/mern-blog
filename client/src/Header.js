5e7f737059c71b88ea77a0feb63bc36a617396e4
Prateek-2022 <patricklucifer9@gmail.com>
2023-08-24 10:37:41 +0530

    context added

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 const Header = () => {
+  const { userInfo, setUserInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+      });
+    });
+  }, []);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
   return (
     <div>
       <header>
@@ -9,8 +31,18 @@ const Header = () => {
           MyBlog
         </Link>
         <nav>
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
+          {username && (
+            <>
+              <Link to="/create">Create new post</Link>
+              <a onClick={logout}>Logout</a>
+            </>
+          )}
+          {!username && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
         </nav>
       </header>
     </div>
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext";
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
   async function login(e) {
     e.preventDefault();
     const response = await fetch("http://localhost:4000/login", {
@@ -14,14 +16,17 @@ const LoginPage = () => {
       credentials: "include",
     });
     if (response.ok) {
-      setRedirect(true);
+      response.json().then((userInfo) => {
+        setUserInfo(userInfo);
+        setRedirect(true);
+      });
     } else {
       alert("wrong credentials");
     }
   }
 
   if (redirect) {
-    console.log(redirect);
+    // console.log(redirect);
     return <Navigate to={"/"} />;
   }
   return (
